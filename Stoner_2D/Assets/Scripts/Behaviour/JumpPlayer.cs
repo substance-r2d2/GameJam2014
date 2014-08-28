fb0daebc900472801be656f3532bd0824bcb6c2e
substance-r2d2 <ananddhavle@gmail.com>
2014-08-29 01:22:29 +0530

    Bheaviour Scripts

---
+++ Stoner_2D/Assets/Scripts/Behaviour/JumpPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPlayer : MonoBehaviour {
+
+  public float maxJumpForce = 300f;
+
+  //Remeber to tag the player as "Player"
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.tag == "Player")
+    {
+      other.gameObject.rigidbody2D.velocity = new Vector2(0, 0);
+      other.gameObject.rigidbody2D.AddForce(new Vector2(0, maxJumpForce));
+    }
+  }
+}
