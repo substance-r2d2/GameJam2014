ad1fa939db565a21377160a3f97369b7d0645294
substance-r2d2 <ananddhavle@gmail.com>
2014-08-29 13:41:16 +0530

    Added xbox controller

--- Stoner_2D/Assets/Scripts/PlayerStateController.cs
+++ Stoner_2D/Assets/Scripts/PlayerStateController.cs
@@ -4,20 +4,20 @@ using System.Collections;
 public class PlayerStateController : MonoBehaviour
 {
 
-  public enum playerStates 
-	{ 
-		rock_move,
-		mud_move,
-		mud_idle, 
-		mud_left, 
-		mud_right, 
-		mud_shoot, 
-		rock_idle,
-		rock_left, 
-		rock_right, 
-		rock_roll, 
-		rock_jump,
-	};
+  public enum playerStates
+  {
+    rock_move,
+    mud_move,
+    mud_idle,
+    mud_left,
+    mud_right,
+    mud_shoot,
+    rock_idle,
+    rock_left,
+    rock_right,
+    rock_roll,
+    rock_jump,
+  };
   public delegate void playerStateHandler(PlayerStateController.playerStates newState);
   public static event playerStateHandler onStateChange;
   public GameObject groundCheck;
@@ -36,23 +36,23 @@ public class PlayerStateController : MonoBehaviour
     float horizontal = Input.GetAxis("Horizontal");
     if (horizontal != 0.0f)
     {
-        switch (PlayerStateListener.m_ePlayerState)
-        {
-          case EPLayerState.ERock:
-            if (onStateChange != null)	
-			   onStateChange(playerStates.rock_move);
+      switch (PlayerStateListener.m_ePlayerState)
+      {
+        case EPLayerState.ERock:
+          if (onStateChange != null)
+            onStateChange(playerStates.rock_move);
           break;
 
-          case EPLayerState.EMud:
-           if (onStateChange != null)          
-			 onStateChange(playerStates.mud_move);
+        case EPLayerState.EMud:
+          if (onStateChange != null)
+            onStateChange(playerStates.mud_move);
           break;
-        }
-     } 
+      }
+    }
     else
     {
-	   switch (PlayerStateListener.m_ePlayerState) 
-     	{
+      switch (PlayerStateListener.m_ePlayerState)
+      {
         case EPLayerState.ERock:
           if (onStateChange != null)
             onStateChange(PlayerStateController.playerStates.rock_idle);
@@ -65,49 +65,56 @@ public class PlayerStateController : MonoBehaviour
       }
     }
 
-    float jump = Input.GetAxis("Jump");
-    if(jump !=0 && grounded)
+    //float jump = Input.GetKeyDown(KeyCode.Joystick1Button0);
+    if ((Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick1Button0)) && grounded)
     {
       grounded = false;
       if (onStateChange != null)
         onStateChange(PlayerStateController.playerStates.rock_jump);
     }
-    if(Input.GetKeyDown(KeyCode.LeftAlt) && grounded)
+    if (Input.GetKeyDown(KeyCode.Joystick1Button2) && grounded)
     {
       if (onStateChange != null)
         onStateChange(PlayerStateController.playerStates.rock_roll);
     }
 
-    if (Input.GetKeyDown(KeyCode.LeftControl))
+    if (Input.GetKeyDown(KeyCode.Joystick1Button4) || Input.GetKeyDown(KeyCode.LeftAlt))
     {
-	  switch (PlayerStateListener.m_ePlayerState)
+      switch (PlayerStateListener.m_ePlayerState)
       {
-		case EPLayerState.ERock:
+        case EPLayerState.ERock:
           if (onStateChange != null)
-					EventHandler.TriggerEvent(EEventID.EVENT_PLAYER_CHANGE_STATE, EPLayerState.EMud);
-          
-          break;
+            EventHandler.TriggerEvent(EEventID.EVENT_PLAYER_CHANGE_STATE, EPLayerState.EMud);
 
-		case EPLayerState.EMud:
+          break;
+      }
+    }
+    if (Input.GetKeyDown(KeyCode.Joystick1Button5) || Input.GetKeyDown(KeyCode.RightAlt))
+    {
+      switch (PlayerStateListener.m_ePlayerState)
+      {
+        case EPLayerState.EMud:
           if (onStateChange != null)
-					EventHandler.TriggerEvent(EEventID.EVENT_PLAYER_CHANGE_STATE, EPLayerState.ERock);
-            
+            EventHandler.TriggerEvent(EEventID.EVENT_PLAYER_CHANGE_STATE, EPLayerState.ERock);
+
           break;
       }
     }
   }
 
+
   void OnCollisionEnter2D(Collision2D other)
   {
-    if(other.gameObject.tag == "Platform")
+    if (other.gameObject.tag == "Platform")
     {
-      foreach(var contact in  other.contacts)
+      foreach (var contact in other.contacts)
       {
         var normal = contact.normal;
-        if (normal.x != 0 || normal.y<0)
+        if (normal.x != 0 || normal.y < 0)
           return;
       }
       grounded = true;
     }
   }
 }
+
--- Stoner_2D/Assets/Scripts/PlayerStateListener.cs
+++ Stoner_2D/Assets/Scripts/PlayerStateListener.cs
@@ -163,7 +163,7 @@ public class PlayerStateListener : MonoBehaviour
 		void MovePlayer ()
 		{
 				float h = Input.GetAxis ("Horizontal");
-
+                Debug.Log(h);
 				anim.SetFloat ("Speed", Mathf.Abs (h));		
 
 				// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
