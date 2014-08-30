003cca45c5c7cf2b7aa506110ff0db4e1159cec0
Jvap <jithin.peter.v@gmail.com>
2014-08-30 09:31:11 +0530

    Merger with v 5.0 from anand

---
+++ Stoner_2D/Assets/BouncyMaterial.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncyMaterial : MonoBehaviour {
+
+  public float xCompForce = 0f;
+  public float yCompForce = 600f;
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if(other.gameObject.tag == "Player")
+    {
+      other.gameObject.rigidbody2D.velocity = new Vector2(0, 0);
+      other.gameObject.rigidbody2D.AddForce(new Vector2(xCompForce, yCompForce));
+    }
+  }
+}
